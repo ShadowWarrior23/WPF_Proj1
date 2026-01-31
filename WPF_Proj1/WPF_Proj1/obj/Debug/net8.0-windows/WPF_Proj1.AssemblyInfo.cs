7d5cdd796128c4ccca451bcc6600e9726ea70916
SHl4yer <gamernagylolhih@gmail.com>
2026-01-31 19:02:50 +0100

    base

--- WPF_Proj1/WPF_Proj1/obj/Debug/net8.0-windows/WPF_Proj1.AssemblyInfo.cs
+++ WPF_Proj1/WPF_Proj1/obj/Debug/net8.0-windows/WPF_Proj1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF_Proj1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff3265b04e467158e5cc3dccdb2e7178ad01bb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF_Proj1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF_Proj1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
