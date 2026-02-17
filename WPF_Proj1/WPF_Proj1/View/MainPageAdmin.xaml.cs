e893c1801162b737efa859f9945cfff1789e2dee
Shadow <gamernagylolhih@gmail.com>
2026-02-18 00:10:54 +0100

    AdminPAgeChanger

--- WPF_Proj1/WPF_Proj1/View/MainPageAdmin.xaml.cs
+++ WPF_Proj1/WPF_Proj1/View/MainPageAdmin.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPF_Proj1.View.UserControls;
 
 namespace WPF_Proj1.View
 {
@@ -19,14 +20,36 @@ namespace WPF_Proj1.View
     /// </summary>
     public partial class MainPageAdmin : Window
     {
+        private readonly UserControl _overviewAdmin = new OverviewAdmin();
+        private readonly UserControl _monthlyMenuAdmin = new MonthlyMenuAdmin();
+        private readonly UserControl _finances = new FinancesAdmin();
         public MainPageAdmin()
         {
             InitializeComponent();
+            PagesAdmin.SelectedIndex = 0;
+            PageAdmin.Content = _overviewAdmin;
         }
 
         private void PageChangerAdmin(object sender, RoutedEventArgs e)
         {
+            int currentPage = PagesAdmin.SelectedIndex;
+            switch (currentPage)
+            {
+                case 0:
+                    PageAdmin.Content = _overviewAdmin;
+                    break;
+                case 1:
+                    PageAdmin.Content = _monthlyMenuAdmin;
+                    break;
 
+                case 2:
+                    PageAdmin.Content = _finances;
+                    break;
+
+                case 3:
+                    MessageBox.Show("Export");
+                    break;
+            }
         }
     }
 }
