319a07ba33bc1443bb6f9bac88153962be54b765
Shadow <gamernagylolhih@gmail.com>
2026-02-18 07:40:57 +0100

    EditStart

---
+++ WPF_Proj1/WPF_Proj1/View/UserControls/UserControls1/DayInMonthAdmin.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Proj1.View.UserControls.UserControls1.UserControls11;
+
+namespace WPF_Proj1.View.UserControls.UserControls1
+{
+    /// <summary>
+    /// Interaction logic for DayInMonthAdmin.xaml
+    /// </summary>
+    public partial class DayInMonthAdmin : UserControl
+    {
+        private readonly UserControl _editMenu = new EditMenu();
+        public DayInMonthAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void Popup(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WPF_Proj1/WPF_Proj1/View/UserControls/UserControls1/UserControls11/EditMenu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Proj1.View.UserControls.UserControls1.UserControls11
+{
+    /// <summary>
+    /// Interaction logic for EditMenu.xaml
+    /// </summary>
+    public partial class EditMenu : UserControl
+    {
+        public EditMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
