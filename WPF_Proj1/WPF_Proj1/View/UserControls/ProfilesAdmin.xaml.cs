f014150f30565ec099e0f1998997b88201cb9195
Bottyán Vak <ecdl01@vbjnet.hu>
2026-03-19 11:33:23 +0100

    ProfDeletion_works

--- WPF_Proj1/WPF_Proj1/View/UserControls/ProfilesAdmin.xaml.cs
+++ WPF_Proj1/WPF_Proj1/View/UserControls/ProfilesAdmin.xaml.cs
@@ -10,27 +10,91 @@ namespace WPF_Proj1.View.UserControls
         public ProfilesAdmin()
         {
             InitializeComponent();
+            LoadProfiles();
+        }
+
+        private void LoadProfiles()
+        {
+            c0.Children.Clear();
+            c1.Children.Clear();
+            c2.Children.Clear();
+
             using var db = new AppDbContext();
 
-            var users = db.Users.Select(u => new {u.FullName, u.Username, u.PasswordHash});
+            var users = db.Users
+                .Where(u => !u.IsAdmin)
+                .Select(u => new
+                {
+                    u.FullName,
+                    u.Username,
+                    u.PasswordHash
+                })
+                .ToList();
 
             int i = 0;
             foreach (var u in users)
             {
-                MessageBox.Show(u.Username);
                 var prof = new Profile();
+
                 prof.Username.Text = u.Username;
                 prof.FullName.Text = u.FullName;
                 prof.Password.Text = u.PasswordHash;
-                //MessageBox.Show($"{u.Username}, {u.FullName}, {u.PasswordHash}");
+
+                prof.UserNameValue = u.Username;
                 prof.Height = 150;
-                //i == 0 ? c0.Children.Add(prof) : i == 1 ? c1.Children.Add(prof) : c2.Children.Add(prof);
-                if (i == 0) c0.Children.Add(prof);
-                else if (i == 1) c1.Children.Add(prof);
+
+                prof.RemoveRequested += Profile_RemoveRequested;
+
+                if (i % 3 == 0) c0.Children.Add(prof);
+                else if (i % 3 == 1) c1.Children.Add(prof);
                 else c2.Children.Add(prof);
-                if (i < 3) prof.Margin = "0 0 150 0";
+
+                if (i < 3) prof.Margin = new Thickness(0, 0, 0, 350);
+                if (i > 5) prof.Margin = new Thickness(0, 350, 0, 0);
+
                 i++;
             }
         }
+
+        private void Profile_RemoveRequested(object? sender, System.EventArgs e)
+        {
+            if (sender is not Profile prof)
+                return;
+
+            using var db = new AppDbContext();
+
+            var user = db.Users.FirstOrDefault(u => u.Username == prof.UserNameValue);
+            if (user != null)
+            {
+                db.Users.Remove(user);
+                db.SaveChanges();
+            }
+
+            LoadProfiles();
+        }
+        /*using var db = new AppDbContext();
+
+
+        //This I'll need later to refresh every time something changes
+        var users0 = db.Users.Select(u => new {u.FullName, u.Username, u.PasswordHash, u.IsAdmin});
+        var users = users0.Where(u => u.IsAdmin == false).ToList();
+
+        int i = 0;
+        foreach (var u in users)
+        {
+            var prof = new Profile();
+            prof.Username.Text = u.Username;
+            prof.FullName.Text = u.FullName;
+            prof.Password.Text = u.PasswordHash;
+            prof.Height = 150;
+            if (i % 3 == 0) c0.Children.Add(prof);
+            else if (i % 3 == 1) c1.Children.Add(prof);
+            else c2.Children.Add(prof);
+            if (i < 3) prof.Margin = new Thickness(0, 0, 0, 350);
+            if (i > 5) prof.Margin = new Thickness(0, 350, 0, 0);
+            i++;
+        }*/
+
+        //If I click the btn in Profile, I want to remove the user (from here of course) from the database and refresh the page
     }
 }
--- WPF_Proj1/WPF_Proj1/View/UserControls/UserControls1/Profile.xaml.cs
+++ WPF_Proj1/WPF_Proj1/View/UserControls/UserControls1/Profile.xaml.cs
@@ -1,12 +1,22 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WPF_Proj1.View.UserControls.UserControls1
 {
     public partial class Profile : UserControl
     {
+        public event EventHandler? RemoveRequested;
+
+        public string UserNameValue { get; set; } = "";
+
         public Profile()
         {
             InitializeComponent();
+        }
+
+        private void RemUser(object sender, System.Windows.RoutedEventArgs e)
+        {
+            RemoveRequested?.Invoke(this, EventArgs.Empty);
         }
     }
 }
